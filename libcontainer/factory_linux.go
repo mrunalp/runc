39a7ab1bbd457c25cf7efb05e84a3b6a8ed145a3
Aleksa Sarai <asarai@suse.de>
2016-06-06 20:26:35 +1000

    libcontainer: refactor syncT handling
    To make the code cleaner, and more clear, refactor the syncT handling
    used when creating the `runc init` process. In addition, document the
    state changes so that people actually understand what is going on.
    
    Rather than only using syncT for the standard initProcess, use it for
    both initProcess and setnsProcess. This removes some special cases, as
    well as allowing for the use of syncT with setnsProcess.
    
    Also remove a bunch of the boilerplate around syncT handling.
    
    This patch is part of the console rewrite patchset.
    
    Signed-off-by: Aleksa Sarai <asarai@suse.de>

--- libcontainer/factory_linux.go
+++ libcontainer/factory_linux.go
@@ -249,11 +249,8 @@ func (l *LinuxFactory) StartInitialization() (err error) {
 		// send it back to the parent process in the form of an initError.
 		// If container's init successed, syscall.Exec will not return, hence
 		// this defer function will never be called.
-		if _, ok := i.(*linuxStandardInit); ok {
-			//  Synchronisation only necessary for standard init.
-			if werr := utils.WriteJSON(pipe, syncT{procError}); werr != nil {
-				panic(err)
-			}
+		if werr := utils.WriteJSON(pipe, syncT{procError}); werr != nil {
+			panic(err)
 		}
 		if werr := utils.WriteJSON(pipe, newSystemError(err)); werr != nil {
 			panic(err)
