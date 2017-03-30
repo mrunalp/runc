aebdd323429f1b466f1122182bf0ae5b46f184f8
Mrunal Patel <mrunalp@gmail.com>
2017-03-30 12:09:34 -0700

    Save config.json + rootfs on create/run for debugging
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- utils.go
+++ utils.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/mrunalp/fileutils"
 	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/urfave/cli"
 )
@@ -18,6 +21,22 @@ func fatal(err error) {
 	os.Exit(1)
 }
 
+// saveState saves the rootfs and config.json used to create/run a container
+func saveState(spec *specs.Spec, configPath string) error {
+	rootfsPath := spec.Root.Path
+	tmpDir, err := ioutil.TempDir("/tmp", "runc-save")
+	if err != nil {
+		return err
+	}
+	if err = fileutils.CopyFile(configPath, tmpDir); err != nil {
+		return err
+	}
+	if err = fileutils.CopyDirectory(rootfsPath, tmpDir); err != nil {
+		return err
+	}
+	return nil
+}
+
 // setupSpec performs initial setup based on the cli.Context for the container
 func setupSpec(context *cli.Context) (*specs.Spec, error) {
 	bundle := context.String("bundle")
@@ -28,6 +47,10 @@ func setupSpec(context *cli.Context) (*specs.Spec, error) {
 	}
 	spec, err := loadSpec(specConfig)
 	if err != nil {
+		return nil, err
+	}
+
+	if err = saveState(spec, filepath.Join(bundle, specConfig)); err != nil {
 		return nil, err
 	}
 	notifySocket := os.Getenv("NOTIFY_SOCKET")
